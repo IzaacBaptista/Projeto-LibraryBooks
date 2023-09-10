6a8d2892cf3cc10fc12c542fa3dc67ccc2330d83
Izaac Baptista <izaacbaptista@gmail.com>
2023-09-10 17:49:15 -0300

    componentizacao

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import './App.css';
-import logo from './imagens/logo.svg'
+import Logo from '../src/componentes/Logo';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <div className='logo'>
-          <img src={logo} alt='logo'></img>
-        </div>
+        <Logo />
       </header>
     </div>
   );
