3e5d7a89108a9cf455864dac1e2fa39f4d862f85
Izaac Baptista <izaacbaptista@gmail.com>
2023-09-19 13:30:40 -0300

    refactor(sistema): refatora componentes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import './App.css';
-import Logo from './componentes/Logo'
-import perfil from './imagens/perfil.svg'
-import sacola from './imagens/sacola.svg'
+import Header from './componentes/Header'
 
-const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
-const icones = [perfil, sacola]
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <Logo></Logo>
-        <ul className='opcoes'>
-          { textoOpcoes.map ( (texto) => (
-              <li className='opcao'><p>{texto}</p></li>
-          ) ) }
-        </ul>
-
-        <ul className='icones'>
-          { icones.map( (icone) => (
-            <li className='icone'><img src={icone}></img></li>
-          )) }
-        </ul>
-      </header>
+      <Header />
     </div>
   );
 }
