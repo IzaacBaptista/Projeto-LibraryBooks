121d98b7d8ebd6c0db9003b273764cacf50cd46a
Izaac Baptista <izaacbaptista@gmail.com>
2023-09-20 07:44:00 -0300

    trocando arquivos css

--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,14 +1,30 @@
-import './estilo.css'
+import styled from 'styled-components'
+
+const OpcoesContainer = styled.ul`
+    display: flex;
+`
+
+const Opcao = styled.li`
+    font-size: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+    padding: 0 5px;
+    cursor: pointer;
+    min-width: 120px;
+`
 
 const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
 
 function OpcoesHeader () {
     return(
-        <ul className='opcoes'>
+        <OpcoesContainer>
           { textoOpcoes.map ( (texto) => (
-              <li className='opcao'><p>{texto}</p></li>
+              <Opcao><p>{texto}</p></Opcao>
           ) ) }
-        </ul>
+        </OpcoesContainer>
     )
 }
 
