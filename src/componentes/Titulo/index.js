76a5c643dc3e58b1fe95d8e0d25aacfb56071b1f
Izaac Baptista <izaacbaptista@gmail.com>
2023-09-22 11:04:22 -0300

    secao ultimos lancamentos

---
+++ src/componentes/Titulo/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Titulo = styled.h2`
+  color: #FFF;
+  font-size: 36px;
+  text-align: center;
+  width: 100%;
+`;
---
+++ src/componentes/UltimosLancamentos/ultimosLancamentos.js
@@ -0,0 +1,7 @@
+import livro from '../../imagens/livro2.png';
+
+export const livros = [
+    {nome: 'Liderança em Design', src: livro, id: 1, autor: 'Cal Newport', categoria: 'produtividade e estilo de vida', paginas: 254},
+    {nome: 'Cangaceiro JavaScript', src: livro, id: 3, autor: 'Flávio Almeida', categoria: 'programação', paginas: 254},
+    {nome: 'Apache Kafka e Spring Boot', src: livro, id: 4, autor: 'Daniel Carvalho', categoria: 'programação', paginas: 254},
+]
