656600b2784f895279485e4fe1063b952875ac0f
Izaac Baptista <izaacbaptista@gmail.com>
2023-09-22 15:51:23 -0300

    create server

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const rotaLivro = require('./routes/livro');
+
+const app = express();
+
+const port = 8000;
+
+app.get('/', (req, res) => {
+    res.send('meu server em node');
+});
+
+app.use('/livros', rotaLivro);
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
