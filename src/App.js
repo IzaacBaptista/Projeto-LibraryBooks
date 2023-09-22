9ab990c2e41976421256af04dfd69011e8449a90
Izaac Baptista <izaacbaptista@gmail.com>
2023-09-22 09:46:03 -0300

    componentes dinamicos

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 
 const AppContainer = styled.div`
     width: 100vw;
-    height: 100vh;
+    height: 100%;
     background-image: linear-gradient(90deg, #002F52 35%, #326589);
 `
 
