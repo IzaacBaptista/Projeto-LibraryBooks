9eb53a24a02b382f231b8add86287a2500e6b49e
IzaacBaptista <izaacbaptista@gmail.com>
2023-09-27 07:29:43 -0300

    criando servico de livros

---
+++ src/servicos/livros.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+// import { response } from 'express';
+
+const livrosAPi = axios.create({
+    baseURL: 'http://localhost:8000/livros',
+});
+
+function getLivros() {
+    const response = livrosAPi.get('/');
+    return response.data;
+}
+
+function getLivro(id) {
+    const response = livrosAPi.get(`/${id}`);
+    return response.data;
+}
+
+function postLivros() {
+    const response = livrosAPi.post('/');
+    return response.data;
+}
+
+function patchLivro(id) {
+    const response = livrosAPi.patch(`/${id}`);
+    return response.data;
+}
+
+function deleteLivros(id) {
+    const response = livrosAPi.delete(`/${id}`);
+    return response.data;
+}
+
+export { getLivros, getLivro, postLivros, patchLivro, deleteLivros };
