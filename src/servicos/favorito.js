52d0c0f23c37d505d353b9b9108f558a3b654280
IzaacBaptista <izaacbaptista@gmail.com>
2023-10-02 08:54:37 -0300

    servico de livros favoritos

---
+++ src/servicos/favorito.js
@@ -0,0 +1,25 @@
+const fs = require("fs")
+
+function getTodosFavoritos() {
+    return JSON.parse(fs.readFileSync("favoritos.json"))
+}
+
+function deletaFavoritoById(id) {
+    const livros = JSON.parse(fs.readFileSync("favoritos.json"))
+    const livrosFiltrados = livros.filter(livro => livro.id !== id)
+    fs.writeFileSync("favoritos.json", JSON.stringify(livrosFiltrados))
+    return livrosFiltrados
+}
+
+function postFavorito(id) {
+    const livros = JSON.parse( fs.readFileSync("../model/livros.json"))
+    const favoritos = JSON.parse(fs.readFileSync("favoritos.json"))
+    const livroInserido = livros.find(livro => livro.id === id)
+    const novaListaDeLivroFavoritos = [...favoritos, livroInserido]
+
+    fs.writeFileSync("favoritos.json", JSON.stringify(novaListaDeLivroFavoritos))
+
+    return novaListaDeLivroFavoritos
+}
+
+module.exports = { getTodosFavoritos, deletaFavoritoById, postFavorito }
