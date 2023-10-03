21e74c8e4a39b021ed73161c0cb2f108c37c631a
IzaacBaptista <izaacbaptista@gmail.com>
2023-10-03 09:05:26 -0300

    usando API de favoritos

--- model/favorito.js
+++ model/favorito.js
@@ -1,23 +1,23 @@
 const fs = require("fs")
 
 function getTodosFavoritos() {
-    return JSON.parse(fs.readFileSync("./src/servicos/favoritos.json"))
+    return JSON.parse(fs.readFileSync("./model/favoritos.json"))
 }
 
 function deletaFavoritoById(id) {
-    const livros = JSON.parse(fs.readFileSync("./src/servicos/favoritos.json"))
+    const livros = JSON.parse(fs.readFileSync("./model/favoritos.json"))
     const livrosFiltrados = livros.filter(livro => livro.id !== id)
-    fs.writeFileSync("./src/servicos/favoritos.json", JSON.stringify(livrosFiltrados))
+    fs.writeFileSync("./model/favoritos.json", JSON.stringify(livrosFiltrados))
     return livrosFiltrados
 }
 
 function insereFavorito(id) {
     const livros = JSON.parse( fs.readFileSync("./src/model/livros.json"))
-    const favoritos = JSON.parse(fs.readFileSync("./src/servicos/favoritos.json"))
+    const favoritos = JSON.parse(fs.readFileSync("./model/favoritos.json"))
     const livroInserido = livros.find(livro => livro.id === id)
     const novaListaDeLivroFavoritos = [...favoritos, livroInserido]
 
-    fs.writeFileSync("./src/servicos/favoritos.json", JSON.stringify(novaListaDeLivroFavoritos))
+    fs.writeFileSync("./model/favoritos.json", JSON.stringify(novaListaDeLivroFavoritos))
 
     return novaListaDeLivroFavoritos
 }
