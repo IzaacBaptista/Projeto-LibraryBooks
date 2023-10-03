bb15457fd70de34d280b3e0b48b74ed6bc05adc8
IzaacBaptista <izaacbaptista@gmail.com>
2023-10-03 07:25:55 -0300

    controller e rota de favoritos

--- routes/livro.js
+++ routes/livro.js
@@ -1,18 +1,18 @@
 const { Router } = require("express")
-const { getLivros, getLivro, postLivros, patchLivro, deleteLivros, getLivrosFavoritos } = require("../controllers/livro")
-
+const { getLivros, getLivro, postLivros, patchLivro, deleteLivros } = require("../controllers/livro")
+const { getFavoritos, postFavorito, removeFavoritoById } = require("../controllers/favorito")
 const router = Router()
 
+//rotas livros
 router.get('/', getLivros)
-
 router.get('/:id', getLivro)
-
-router.get('/favoritos', getLivrosFavoritos)
-
 router.post('/', postLivros)
-
 router.patch('/:id', patchLivro)
-
 router.delete('/:id', deleteLivros)
+
+//rotas favoritos
+router.get('/favoritos', getFavoritos)
+router.post('/favoritos', postFavorito)
+router.delete('/favoritos/:id', removeFavoritoById)
 
 module.exports = router
